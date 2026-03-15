da63f15467296b305eb51149bf68df67e9bf009e
Olagunju Ayomide <olagunjuayomide02@gmail.com>
2026-03-15 08:15:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,18 +29,20 @@ model = load_model()
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+  image = Image.open(uploaded_file).convert("RGB")
+  st.image(image, caption="Uploaded Image", use_column_width=True)
+  image = image.resize((32,32))
+  img_array = np.array(image) / 255.0
+  img_array = np.expand_dims(img_array, axis=0)
+  prediction = model.predict(img_array)
+  predicted_class = class_names[np.argmax(prediction)]
+  confidence = np.max(prediction)
+  st.success(f"Prediction: {predicted_class}")
+  st.write(f"Confidence: {confidence*100:.2f}%")
+  
 
-#Preprocess image
-image = image.resize((32,32))
-img_array = np.array(image) / 255.0
-img_array = np.expand_dims(img_array, axis=0)
 
-#predict
-prediction = model.predict(img_array)
-predicted_class = class_names[np.argmax(prediction)]
-confidence = np.max(prediction)
 
-st.success(f"Prediction: {predicted_class}")
-st.write(f"Confidence: {confidence*100:.2f}%")
+
+
+
